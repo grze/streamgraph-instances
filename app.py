95e57c2bf1539abc80b3bc2f5c025a9fba734e48
chris grzegorczyk <grze@eucalyptus.com>
2016-03-26 20:18:09 -0700

    Changes for making work.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 import tornado.ioloop
 import tornado.web
+from datetime import date, timedelta, datetime
+
 
 from tornado.options import options
 from tornado.httpserver import HTTPServer
@@ -13,11 +15,11 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def prepare(self):
         self.connection = psycopg2.connect(
-            host='*',
-            port='*',
-            user='*',
-            database='*',
-            password='*')
+            host='grzegraph.ckseu9rplkxc.us-west-1.rds.amazonaws.com',
+            port='5432',
+            user='ashamblin',
+            database='ashamblin',
+            password='zoomzoom')
         self.cursor = self.connection.cursor()
 
 
@@ -35,30 +37,69 @@ class DataHandler(BaseHandler):
 
     @tornado.gen.coroutine
     def get(self, date=None):
+        datet = datetime.strptime(date,"%Y-%m-%d");
+        enddatetime = datet + timedelta(days=-2)
+        enddate = enddatetime.strftime("%Y-%m-%d")
+        print "%s %s" % (date,enddate)
 
-        self.cursor.execute('''
-            select  cloudname,
-                    corecount,
-                    launchtime
-            from dupa
-            where CAST(launchtime as DATE) = %s
-            order by launchtime
-        ''', (date,))
-
+        query = '''
+            SELECT SUM(corecount),launchtime,cloudname FROM (
+                SELECT a.launchtime as launchtime, b.cloudname as cloudname,
+                        CASE WHEN (a.corecount IS NULL OR a.cloudname != b.cloudname ) THEN 0 ELSE a.corecount END AS corecount
+                        FROM ( SELECT DISTINCT(cloudname) FROM dupa WHERE CAST(launchtime as DATE) < %s ) as b, dupa as a
+                WHERE CAST(launchtime as DATE) < %s
+                AND CAST(launchtime as DATE) > %s
+                ORDER BY launchtime,cloudname,corecount DESC 
+            ) as result
+            GROUP BY result.launchtime, result.cloudname;
+        '''
+        query = '''
+        SELECT SUM(corecount), hour, cloudname FROM (
+            SELECT a.launchtime as launchtime, EXTRACT(hour from launchtime) as hour, b.cloud as cloudname,
+                    CASE WHEN (a.corecount IS NULL OR a.cloud != b.cloud ) THEN 0 ELSE a.corecount END AS corecount
+            FROM 
+                ( SELECT DISTINCT(cloud) FROM instancehistory WHERE CAST(launchtime as DATE) = %s ) as b, instancehistory as a
+            WHERE CAST(launchtime as TIMESTAMP) < %s 
+            AND CAST(launchtime as TIMESTAMP) >= %s  ORDER BY launchtime,cloudname,corecount DESC 
+        ) as result
+        GROUP BY result.hour, cloudname
+        ORDER BY result.hour
+        '''
+        query = '''
+SELECT SUM(corecount), bucket, cloudname FROM (
+    SELECT a.launchtime as launchtime, 
+            EXTRACT(hour from launchtime) as hour, 
+            EXTRACT(minute from launchtime) as minute, 
+            trunc((EXTRACT(hour from launchtime)*60+EXTRACT(minutes from launchtime)) / 6) as bucket, 
+            b.cloud as cloudname,
+            CASE WHEN (a.corecount IS NULL OR a.cloud != b.cloud ) THEN 0 ELSE a.corecount END AS corecount
+    FROM 
+        ( SELECT DISTINCT(cloud) FROM instancehistory WHERE CAST(launchtime as DATE) = '2014-04-01' ) as b, instancehistory as a
+    WHERE CAST(launchtime as TIMESTAMP) < '2014-04-01 23:59:00' 
+    AND CAST(launchtime as TIMESTAMP) >= '2014-03-31 00:00:00'  ORDER BY launchtime,cloudname,corecount DESC 
+) as result
+GROUP BY result.bucket, cloudname
+ORDER BY cloudname, result.bucket
+        '''
+        print query
+        self.cursor.execute(query, (date,date,enddate,))
         instances = self.cursor.fetchall()
 
         output = {}
         for instance in instances:
-            (cloud, coresonline, launchtime) = instance
+            (coresonline, launchtime, cloud) = instance
 
             if cloud not in output:
                 output[cloud] = []
 
-            output[cloud].append({
+            newval = {
                 'y': coresonline,
-                'x': launchtime.isoformat()
-            })
-        output = output.values()
+                'x': launchtime.isoformat() if not launchtime.is_integer() else launchtime
+            }
+            print "%s %s" % (cloud,newval)
+            output[cloud].append(newval)
+
+        # output = output.values()
 
         self.set_header('Content-type', 'application/json')
         self.write({'instances': output})
--- assets/js/stream.js
+++ assets/js/stream.js
@@ -1,81 +1,195 @@
-angular.module('EucaInstanceHistory', [])
-.directive('historyChart', ['InstanceService', function (InstanceService) {
-    return {
-        scope: {
-            rangeStart: '@',
-            rangeEnd: '@'
-        },
-        link: function (scope, element) {
-            scope.target = element;
-        },
-        controller: ['$scope', '$timeout', function ($scope, $timeout) {
-            var start = new Date(2014, 2, 1),//new Date($scope.rangeStart),
-                end = new Date($scope.rangeEnd);
-
-            var formattedDate = [start.getFullYear(), start.getMonth() + 1, start.getDate()].join('-');
-
-            $scope.chart = d3.select($scope.target);
-            $scope.stack = d3.layout.stack().offset('wiggle');
-
-            $scope.getData = function () {
-            };
-
-            $scope.getChart = function (result) {
-                var values = result.data.instances;
-                var layer = $scope.stack(values);
-
-                var xScale = d3.time.scale()
-                    .domain([values[0].launchtime, values[values.length - 1].launchtime])
-                    .range([0, 900]);
-
-                var yScale = d3.scale.linear()
-                    .domain([0, values.reduce(function (prev, curr) {
-                        return prev + curr.corecount;
-                    }, 0)])
-                    .range([500, 0]);
-
-                var color = d3.scale.linear()
-                    .range(["#aad", "#556"]);
-
-                var area = d3.svg.area()
-                    .x(function (d) {
-                        return xScale(d.x);
-                    })
-                    .y(function (d) {
-                        return yScale(d.y);
-                    })
-                    .y0(function (d) {
-                        return yScale(d.y + d.y0);
-                    });
-
-                $scope.chart.selectAll('path')
-                    .data(layer)
-                    .enter().append('path')
-                    .attr('d', area)
-                    .style("fill", function() {
-                        return color(Math.random());
-                    });
-            };
-
-            $timeout(function () {
-                InstanceService.getInstances(formattedDate).then(function (result) {
-                    $scope.getChart(result);
-                }, function (response) {
-                    console.log('error', response);
-                });
-            });
-        }]
-    };
-}])
-.factory('InstanceService', ['$http', '$interpolate', function ($http, $interpolate) {
-    var urlFunc = $interpolate('/data/{{ date }}');
-
-    return {
-        getInstances: function (date) {
-            return $http({
-                method: 'GET',
-                url: urlFunc({ date: date })
-            });
+angular.module('InstanceHistory', ['ngMaterial','ngAria'])
+    .controller('transitionHistory', function($scope) {
+        $scope.title1 = 'Button';
+        $scope.title4 = 'Warn';
+        $scope.isDisabled = false;
+        $scope.googleUrl = 'http://google.com';
+        console.log('error',$scope);
+        if($scope.transition) {
+            $scope.transition();
         }
-    };
-}]);
+    })
+    .directive('historyChart', ['InstanceService',
+        function(InstanceService) {
+            return {
+                scope: {
+                    rangeStart: '@',
+                    rangeEnd: '@'
+                },
+                link: function(scope, element) {
+                    scope.target = element;
+                },
+                controller: ['$scope', '$timeout',
+                    function($scope, $timeout) {
+                        console.log('error',$scope);
+
+                        var start = new Date(2014, 7, 2), //new Date($scope.rangeStart),
+                            end = new Date(2015, 7, 2); //new Date($scope.rangeEnd);
+
+                        var formattedDate = [start.getFullYear(), start.getMonth(), start.getDate()].join('-');
+                        var formattedEndDate = [end.getFullYear(), end.getMonth(), end.getDate()].join('-');
+
+
+                        $scope.getData = function() {};
+
+                        $scope.getChart = function(result, resultEnd) {
+                            var stack = d3.layout.stack()
+                                .offset('wiggle')
+                                .order('reverse');
+                            console.log('error', result.data.instances);
+                            console.log('error', resultEnd.data.instances);
+
+                            var vals = Object.getOwnPropertyNames(result.data.instances).map(function(x) {
+                                return result.data.instances[x].map(
+                                    function(d, i) {
+                                        return {
+                                            x: new Date(d.x).getTime(),
+                                            y: d.y
+                                        }
+                                    });
+                            });
+                            vals = vals.map(function(d) {
+                                localMin = d3.min(d, function(item) {
+                                    return item.y
+                                })
+                                return d.map(
+                                    function(item) {
+                                        item.y = item.y - localMin;
+                                        return item
+                                    })
+                            })
+                            console.log('error', vals);
+                            $scope.layers = stack(vals);
+                            console.log('error', $scope.layers)
+
+
+                            var endVals = Object.getOwnPropertyNames(resultEnd.data.instances).map(function(x) {
+                                return resultEnd.data.instances[x].map(
+                                    function(d, i) {
+                                        return {
+                                            x: new Date(d.x).getTime(),
+                                            y: d.y
+                                        }
+                                    });
+                            });
+                            endVals = endVals.map(function(d) {
+                                localMin = d3.min(d, function(item) {
+                                    return item.y
+                                })
+                                return d.map(
+                                    function(item) {
+                                        item.y = item.y - localMin;
+                                        return item
+                                    })
+                            })
+                            console.log('error', endVals);
+                            $scope.layersEnd = stack(endVals);
+                            console.log('error', $scope.layersEnd)
+
+                            var width = 1200,
+                                height = 600;
+
+                            var startTime = d3.min(vals.map(function(d) {
+                                    return d3.min(d, function(item) {
+                                        return item.x
+                                    })
+                                })),
+                                endTime = d3.max(vals.map(function(d) {
+                                    return d3.max(d, function(item) {
+                                        return item.x
+                                    })
+                                }));
+
+                            // var xScale = d3.time.scale()
+                            var xScale = d3.scale.linear()
+                                .domain([startTime, endTime])
+                                .range([0, width]);
+
+                            var maxCores = d3.max(vals.map(function(d) {
+                                return d3.max(d, function(item) {
+                                    return item.y0
+                                })
+                            }))
+                            var minCores = d3.min(vals.map(function(d) {
+                                return d3.min(d, function(item) {
+                                    return item.y0
+                                })
+                            }))
+                            var yScale = d3.scale.linear()
+                                .domain([minCores, maxCores])
+                                .range([height, 0]);
+
+                            var color = d3.scale.linear()
+                                // .range(["#fff", "#f90"]);
+                                // .range(["#08f", "#f90"]);
+                                .range(["#08f", "#fff"]);
+
+                            var svg = d3.select($scope.target[0]);
+
+                            $scope.area = d3.svg.area()
+                                .x(function(d) {
+                                    return xScale(d.x);
+                                })
+                                .y0(function(d) {
+                                    return yScale(d.y0);
+                                })
+                                .y1(function(d) {
+                                    return yScale(d.y0 + d.y);
+                                });
+
+                            svg.selectAll('path')
+                                .data($scope.layers)
+                                .enter().append('path')
+                                .attr('d', $scope.area)
+                                .style("fill", function() {
+                                    return color(Math.random());
+                                });
+                        };
+
+                        $scope.$parent.transition = function() {
+                            console.log('warning','transitioning');
+                            d3.selectAll("path")
+                                .data(function() {
+
+                                    var d = $scope.layersEnd;
+                                    $scope.layersEnd = $scope.layers;
+                                    return $scope.layers = d;
+                                })
+                                .transition()
+                                .duration(2500)
+                                .attr("d", $scope.area);
+                            console.log('warning','transitioning done');
+                        }
+
+                        $timeout(function() {
+                            InstanceService.getInstances(formattedDate).then(function(result) {
+                                InstanceService.getInstances(formattedEndDate).then(function(resultEnd) {
+                                    $scope.getChart(result, resultEnd);
+                                }, function(response) {
+                                    console.log('error', response);
+                                });
+                            }, function(response) {
+                                console.log('error', response);
+                            });
+                        });
+                    }
+                ]
+            };
+        }
+    ])
+    .factory('InstanceService', ['$http', '$interpolate',
+        function($http, $interpolate) {
+            var urlFunc = $interpolate('/data/{{ date }}');
+
+            return {
+                getInstances: function(date) {
+                    return $http({
+                        method: 'GET',
+                        url: urlFunc({
+                            date: date
+                        })
+                    });
+                }
+            };
+        }
+    ]);
